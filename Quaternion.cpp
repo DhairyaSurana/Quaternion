14f561557e315a3f15a9580e6ec861bab71fdb01
Dhairya Surana <dhairs7@vt.edu>
2020-09-19 06:28:46 -0400

    Added starter code

--- Quaternion.cpp
+++ Quaternion.cpp
@@ -1,1 +1,29 @@
+#include "Quaternion.h"
+#include <iostream> 
 
+
+Quaternion::Quaternion(int s, int i, int j, int k) {}
+
+double::Quaternion getRe() {}
+double::Quaternion norm() {}
+double::Quaternion getIm(int n) {}
+
+Quaternion::Quaternion getIm() {}
+Quaternion::Quaternion conj() {}
+Quaternion::Quaternion getUnitVector() {}
+
+operator::Quaternion +(const Quaternion& q) {}
+operator::Quaternion -(const Quaternion& q) {}
+operator::Quaternion *(const Quaternion& q) {}
+operator::Quaternion -() const {}
+
+
+std::ostream& operator::Quaternion << (std::ostream & os, const Quaternion & q) {}
+
+
+int main() {
+
+	std::cout << "Hello World!";
+
+	return 0;
+}
--- Quaternion.h
+++ Quaternion.h
@@ -1,1 +1,49 @@
+#include <iostream>
+#include <vector>
 
+class Quaternion {
+
+	private:
+
+		int scalar;
+		std::vector <int> imag;
+
+	public:
+
+		// Constructor
+		Quaternion(int s, int i, int j, int k);
+
+		// Returns the real part of quaternion
+		double getRe();
+
+		// Returns the euclidien norm of quaternion
+		double norm();
+
+		// Returns the nth imaginary part of quaternion
+		double getIm(int n);
+		
+		// Returns a quaternion with only the imaginary parts (real part is 0)
+		Quaternion getIm();
+
+		// Returns the complex conjugate of quaternion 
+		Quaternion conj();
+
+		// Returns the unit vector of quaternion
+		Quaternion getUnitVector();
+
+		// addition
+		Quaternion operator +(const Quaternion& q);
+
+		// substraction
+		Quaternion operator -(const Quaternion& q);
+
+		// multiplication
+		Quaternion operator *(const Quaternion& q);
+
+		// negation
+		Quaternion operator -() const;
+
+
+		friend std::ostream& operator<<(std::ostream& os, const Quaternion& q);
+
+};
